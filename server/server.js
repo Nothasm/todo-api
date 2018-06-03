a6d3f0f354c151c80a24f6b9812ef365a82f0f92
Tom <toms.matos@gmail.com>
2018-06-02 22:38:13 -0300

    get route

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {mongoose} = require("./db/mongoose");
+const { mongoose } = require("./db/mongoose");
 
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
 var app = express();
 
@@ -20,10 +20,16 @@ app.post('/todos', (req, res) => {
     }, (err) => {
         res.status(400).send(err);
     });
-})
-    .get('/todos', (req, res) => {
-        Todo.find().then((result) => res.send(result));
-    })
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((results) => {
+        res.send({
+            status: "OK",
+            results
+        });
+    }, (err) => res.status(400).send(err));
+});
 
 app.listen(3000, () => {
     console.log("Server up on port 3000")
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,19 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+var todos = [
+    {
+        text: "Primeiro todo"
+    },
+    {
+        text: "Segundo todo"
+    }
+];
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        Todo.insertMany(todos).then(() => done());
+    });
 });
 
 describe('POST /todos', () => {
@@ -22,7 +33,7 @@ describe('POST /todos', () => {
                 if (err) {
                     return done(err);
                 }
-                Todo.find().then((todos) => {
+                Todo.find({ text: 'Test todo text'}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -41,11 +52,24 @@ describe('POST /todos', () => {
                     return done(err);
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
                     done();
                 })
                 .catch((e) => done(e));
             })
+    });
+});
+
+describe('GET /todos', () => {
+    it('should return all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.status).toBe('OK');
+                expect(res.body.results.length).toBe(2);
+            })
+            .end(done);
     });
 });
 
